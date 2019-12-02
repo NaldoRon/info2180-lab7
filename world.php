94fe70615629469887dba5e251548c3f153317af
NaldoRon <49801496+NaldoRon@users.noreply.github.com>
2019-12-01 23:44:59 -0500

    Lab

---
+++ world.php
@@ -0,0 +1,18 @@
+<?php
+$myCountry = $_GET['country'];
+$host = getenv('IP');
+$username = 'lab7_user';
+$password = '';
+$dbname = 'world';
+
+$conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+$stmt = $conn->query("SELECT * FROM countries");
+
+$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+?>
+<ul>
+<?php foreach ($results as $row): ?>
+  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
+<?php endforeach; ?>
+</ul>
